caff86aeb95a2430cea75ed6afd67a7989f03dcc
KumarAmrendram <amrendramkumar@gmail.com>
2022-08-10 01:35:40 +0530

    cards added to publisher and advertisers

--- app.js
+++ app.js
@@ -23,11 +23,12 @@ new fullpage("#fullpage", {
     tl.from('.partner-li', {y:-200, stagger: 0.2, opacity:0, translate:"translateY(100px)"}, "-=1")
     tl.from('.join-li', {y:-200, stagger: 0.2, opacity:0, translate:"translateY(100px)"}, "-=1.5")
     tl.from(".earn-para", { x: 100, opacity: 0, delay: 0.5, duraton: 1.5 }, "-=1.5");
-    tl.from('.brand-img-top', {y:-200, stagger: 0.2, opacity:0, translate:"translateY(100px)"}, "-=1.5")
-    tl.from('.brand-img-bottom', {y:200, stagger: 0.2, opacity:0, translate:"translateY(100px)"}, "-=1.5")
-    tl.from(".client-h1", { x: -100, opacity: 0, delay: 0.5, duraton: 1.5 });
     tl.from('.mission-img', {x:200, stagger: 0.2, opacity:0, translate:"translateY(100px)"}, "-=1")
-    tl.from('.box', {y:-200, stagger: 0.2, opacity:0, translate:"translateY(5px)"}, "-=1.5")
+    tl.from('.box, .brand-box', {x:200, stagger: 0.2, opacity:0, translate:"translateY(5px)"}, "-=1.5")
+    tl.from('.brand-img', {x:-200, stagger: 0.2, opacity:0, translate:"translateY(50px)"}, "-=1.5")
+    tl.from('.icon', {y:-200, stagger: 0.3, opacity:0, translate:"translateY(5px)"}, "-=1.5")
+    tl.from('.advertiser-card, advertiser-heading', {x:200, stagger: 0.2, opacity:0, translate:"translateY(5px)"}, "-=1.5")
+    
   },
   BeforeLeave: (origin, destination, direction) => {
     const section = destination.item;
