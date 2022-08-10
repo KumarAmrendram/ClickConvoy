810a679d9f361174712c98f48a74d852956497db
KumarAmrendram <amrendramkumar@gmail.com>
2022-08-10 23:10:08 +0530

    rb

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ new fullpage("#fullpage", {
     });
     tl.from("h1, button", { x: -100, opacity: 0, delay: 0.5, duraton: 1.5 });
     tl.from('.icon, .banner-text', {x:200, opacity:0, delay: 0.5, translate:"translateX(50px)"}, "-=1.5")
-    tl.from(".earn-para", { x:100, opacity: 0, delay: 0.5, duraton: 1.5 }, "-=1");
     tl.from('.mission-img', {x:200, stagger: 0.2, opacity:0, translate:"translateY(100px)"}, "-=1")
     tl.from('.brand-box', {x:200, stagger: 0.2, opacity:0, translate:"translateY(5px)"}, "-=0.5")
     tl.from('.brand-img', {x:-200, stagger: 0.2, opacity:0, translate:"translateY(50px)"}, "-=1.5")
