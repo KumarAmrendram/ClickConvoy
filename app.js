23b2d8b06daad8fa86300b8cecd38e8cdb5d8c08
KumarAmrendram <amrendramkumar@gmail.com>
2022-08-10 15:05:56 +0530

    adv from index.js

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 new fullpage("#fullpage", {
   autoScrolling: true,
-  scrollOverflow: true,
-  navigaton: true,
   keyboardScrolling: true,
-  fixedElements: '.navigation',
   loopTop: true,
   loopBottom: true,
 	fixedElements: '.navigation, .social',
@@ -21,11 +18,11 @@ new fullpage("#fullpage", {
     });
     tl.from("h1, button", { x: -100, opacity: 0, delay: 0.5, duraton: 1.5 });
     // tl.from("h4", { stagger: 0.2, x: 100, opacity: 0, duraton: 1.5 }, "-=1");
-    tl.from('.partner-li', {y:-200, stagger: 0.2, opacity:0, translate:"translateY(100px)"}, "-=1")
-    tl.from('.join-li', {y:-200, stagger: 0.2, opacity:0, translate:"translateY(100px)"}, "-=1.5")
-    tl.from(".earn-para", { x: 100, opacity: 0, delay: 0.5, duraton: 1.5 }, "-=1.5");
+    // tl.from('.partner-li', {y:-200, stagger: 0.2, opacity:0, translate:"translateY(100px)"}, "-=1")
+    // tl.from('.join-li', {y:-200, stagger: 0.2, opacity:0, translate:"translateY(100px)"}, "-=1.5")
+    tl.from(".earn-para", { x:100, opacity: 0, delay: 0.5, duraton: 1.5 }, "-=1.5");
     tl.from('.mission-img', {x:200, stagger: 0.2, opacity:0, translate:"translateY(100px)"}, "-=1")
-    tl.from('.box, .brand-box', {x:200, stagger: 0.2, opacity:0, translate:"translateY(5px)"}, "-=1.5")
+    tl.from('.box, .brand-box, .publisher-box', {x:200, stagger: 0.2, opacity:0, translate:"translateY(5px)"}, "-=1.5")
     tl.from('.brand-img', {x:-200, stagger: 0.2, opacity:0, translate:"translateY(50px)"}, "-=1.5")
     tl.from('.icon', {y:-200, stagger: 0.3, opacity:0, translate:"translateY(5px)"}, "-=1.5")
     tl.from('.advertiser-card, advertiser-heading', {x:200, stagger: 0.2, opacity:0, translate:"translateY(5px)"}, "-=1.5")
