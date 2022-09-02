b518542b9de584b792eec35b235726da9bf548ef
KumarAmrendram <amrendramkumar@gmail.com>
2022-09-02 21:58:41 +0530

    rb

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ new fullpage("#fullpage", {
   keyboardScrolling: true,
   loopBottom: true,
 	fixedElements: '.navigation, .social',
+  lazyLoading: true,
 
   onLeave: (origin, destination, direction) => {
     const section = destination.item;
@@ -16,14 +17,14 @@ new fullpage("#fullpage", {
       },
     });
     tl.from("h1, button", { x: -100, opacity: 0, delay: 0.5, duraton: 1.5 });
-    tl.from('.icon, .banner-text', {x:200, opacity:0, delay: 0.5, translate:"translateX(50px)"}, "-=1.5")
-    tl.from('.mission-img', {x:200, stagger: 0.2, opacity:0, translate:"translateY(100px)"}, "-=1.3")
-    tl.from('.brand-box', {x:200, stagger: 0.2, opacity:0, translate:"translateY(5px)"}, "-=1.5")
-    tl.from('.brand-img', {x:-200, stagger: 0.2, opacity:0, translate:"translateY(50px)"}, "-=2")
-    tl.from('.adv-img', {y:-200, stagger: 0.2, opacity:0, translate:"translateX(50px)"}, "-=2")
-    tl.from('.pub-box, .join-li', {y:200, stagger: 0.2, opacity:0, translate:"translateY(5px)"}, "-=2.6")
-    tl.from('.pub-img', {x:-200, stagger: 0.2, opacity:0, translate:"translateY(5px)"}, "-=2.7") 
-    tl.from(".mission-para",{opacity: 0, delay: 0.5, duraton: 1.5 }, "-=2");
+    tl.from('.icon, .banner-text', {x:200, opacity:0, delay: 0.5}, "-=1.5")
+    tl.from('.mission-img', {x:200, stagger: 0.2, opacity:0}, "-=1.5")
+    tl.from('.brand-box', {x:200, stagger: 0.2, opacity:0}, "-=1.5")
+    tl.from('.brand-img', {x:-200, stagger: 0.2, opacity:0, translate:"translateY(50px)"}, "-=1.5")
+    tl.from('.adv-img', {y:-200, stagger: 0.2, opacity:0}, "-=1.5")
+    tl.from('.pub-box, .join-li', {y:200, stagger: 0.2, opacity:0}, "-=1.5")
+    tl.from('.pub-img', {x:-200, stagger: 0.2, opacity:0}, "-=1.5") 
+    tl.from(".mission-para",{opacity: 0, delay: 0.5, duraton: 1.5 }, "-=1.5");
   },
   BeforeLeave: (origin, destination, direction) => {
     const section = destination.item;
@@ -36,9 +37,6 @@ new fullpage("#fullpage", {
         toggleActions: "restart none none none",
       },
     });
-    tl.to("h1, button", { x: -100, opacity: 1, delay: 0.2, duraton: 0.5 });
-    tl.to('.brand-img-bottom', {y:200, opacity:0, translate:"translateY(100px)"}, "-=1.5")
-    tl.to('.brand-img-top', {y:-200, opacity:0, translate:"translateY(100px)"}, "-=1.5")
-    tl.to(".mission-para",{opacity: 1, delay: 0.5, duraton: 1.5 }, "-=2");
+    tl.to(".section", { x: -1000, opacity: 0, delay: 0, duraton: 0.5 });
   },
 }); 
